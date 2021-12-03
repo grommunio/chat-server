42cbf8f4db794a02b8ff65156471b2cd06721728
Victor Tesla <victor.tesla@grammm.com>
2021-12-03 12:14:01 +0100

    config: enable local unix socket at a different location

--- model/config.go
+++ model/config.go
@@ -238,7 +238,7 @@ const (
 	CloudSettingsDefaultCwsAPIURL = "https://portal.internal.prod.cloud.mattermost.com"
 	OpenidSettingsDefaultScope    = "profile openid email"
 
-	LocalModeSocketPath = "/var/tmp/mattermost_local.socket"
+	LocalModeSocketPath = "/var/tmp/grommunio-chat_local.socket"
 )
 
 func GetDefaultAppCustomURLSchemes() []string {
@@ -748,7 +748,7 @@ func (s *ServiceSettings) SetDefaults(isUpdate bool) {
 	}
 
 	if s.EnableLocalMode == nil {
-		s.EnableLocalMode = NewBool(false)
+		s.EnableLocalMode = NewBool(true)
 	}
 
 	if s.LocalModeSocketLocation == nil {
